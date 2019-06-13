8ef9162e6839242b8d898c2030ed189a7d6e6abd
Glitch (mydash) <none>
2019-06-13 00:28:42 +0000

    Initial Commit

---
+++ public/js/script.js
@@ -0,0 +1,19 @@
+
+function enviar(){
+  document.getElementByID('enviar_MSG').sumit();
+  alert('Mensaje enviado');
+}
+
+$(".dropdown-trigger").dropdown();
+$(document).ready(function(){
+   $('.tabs').tabs();
+});
+
+$(document).ready(function(){
+  $('.modal').modal();
+});
+ $(document).ready(function(){
+    $('select').formSelect();
+  });
+
+  //ok buen hombre. PD: soy free de la hoya :v PD : notDeveloper.. tambien se puede :v
